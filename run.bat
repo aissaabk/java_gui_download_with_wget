f191445263b0687042ec396c11e7ebc036e25efa
belabes mellik <belmelahmed@gmail.com>
2025-08-29 23:30:00 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,19 +1,39 @@
 @echo off
 setlocal
-set APP_NAME=WgetGuiApp
 set FLATLAF_VER=3.4.1
 set FLATLAF_JAR=flatlaf-%FLATLAF_VER%.jar
+set LIB_PATH=lib\%FLATLAF_JAR%
 
-where java >nul 2>&1
-if errorlevel 1 (
-  echo [ERROR] Java Runtime not found. Please install JRE/JDK 17+.
-  exit /b 1
-)
-
-if not exist lib\%FLATLAF_JAR% (
+if not exist %LIB_PATH% (
   echo Downloading FlatLaf...
-  powershell -Command "Invoke-WebRequest -Uri https://repo1.maven.org/maven2/com/formdev/flatlaf/%FLATLAF_VER%/flatlaf-%FLATLAF_VER%.jar -OutFile lib\%FLATLAF_JAR%"
+  powershell -Command "Invoke-WebRequest -Uri https://repo1.maven.org/maven2/com/formdev/flatlaf/%FLATLAF_VER%/flatlaf-%FLATLAF_VER%.jar -OutFile %LIB_PATH%"
 )
 
-echo Running %APP_NAME%...
-java -cp "dist\%APP_NAME%.jar;lib\%FLATLAF_JAR%" WgetGuiApp
+:MENU
+echo.
+echo ============================
+echo   Select Application to Run
+echo ============================
+echo [1] WgetManager
+echo [2] WgetGuiApp
+echo [0] Exit
+echo.
+set /p choice=Enter choice: 
+
+if "%choice%"=="1" goto RUN_MANAGER
+if "%choice%"=="2" goto RUN_GUI
+if "%choice%"=="0" exit /b
+goto MENU
+
+:RUN_MANAGER
+echo Running WgetManager...
+java -cp "dist\WgetManager.jar;lib\%FLATLAF_JAR%" WgetManager
+goto END
+
+:RUN_GUI
+echo Running WgetGuiApp...
+java -cp "dist\WgetGuiApp.jar;lib\%FLATLAF_JAR%;resources" WgetGuiApp
+goto END
+
+:END
+pause
